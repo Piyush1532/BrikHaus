258225706da48739a4d8ba6d628df4a08117a64c
Piyush1532 <piyushadav503@gmail.com>
2025-02-09 15:37:43 +0530

    initial commit

---
+++ src/assets/assets.js
@@ -0,0 +1,94 @@
+import brand_img from "./brand_img.png"
+import cross_icon from "./cross_icon.svg"
+import header_img from "/header_img.png"
+import left_arrow from "./left_arrow.svg"
+import menu_icon from "./menu_icon.svg"
+import right_arrow from "./right_arrow.svg"
+import star_icon from "./star_icon.svg"
+import profile_img_1 from "./profile_img_1.png"
+import profile_img_2 from "./profile_img_2.png"
+import profile_img_3 from "./profile_img_3.png"
+import brikhaus_logo from "./brikhaus_logo.png"
+
+
+
+export const assets={
+    brand_img,
+    cross_icon,
+    header_img,
+    left_arrow,
+    right_arrow,
+    star_icon,
+    profile_img_1,
+    profile_img_2,
+    profile_img_3,
+    brikhaus_logo,
+    menu_icon
+}
+
+
+export  const projectData=[
+    {
+        title:"Skyline Haven",
+        price:"$20,00,000",
+        location:"California",
+    },
+    {
+        title:"Vista Verde",
+        price:"$24,00,000",
+        location:"San Francisco",
+    },
+    {
+        title:"Serinity Suites",
+        price:"$24,50,000",
+        location:"Chicago",
+    },
+    {
+        title:"Central Square",
+        price:"$30,00,000",
+        location:"Los Angeles",
+    }
+    ,{
+        title:"Barton Square",
+        price:"$20,80,000",
+        location:"New York",
+    },
+    {
+        title:"SandPiper Estates",
+        price:"$10,50,000",
+        location:"New Jersey",
+    }
+]
+
+
+ export const testimonialsData = [
+    {
+      name: "Liam Carter",
+      job: "Software Engineer",
+      alt: "Liam Carter - Software Engineer",
+      rating: 4.8,
+      text: "Finding my dream home was seamless with this agency! Their professionalism and deep market knowledge made the entire process stress-free. Highly recommend!",
+      img: "",
+    },
+    {
+      name: "Mark Reynolds",
+      job: "Marketing Specialist",
+      alt: "Sophia Reynolds - Marketing Specialist",
+      rating: 5.0,
+      text: "Exceptional service! The agents listened to my needs and found me the perfect apartment in no time. They truly go above and beyond for their clients.",
+      img: "",
+    },
+    {
+      name: "Daniel Foster",
+      job: "Financial Analyst",
+      alt: "Daniel Foster - Financial Analyst",
+      rating: 4.7,
+      text: "A smooth and hassle-free experience! The team was transparent, professional, and helped me make a great investment in real estate.",
+      img: "",
+    },
+  ];
+  
+
+  
+  
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './navbar'
+
+const Header = () => {
+  return (
+    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' style={{backgroundImage:"url('https://images.unsplash.com/photo-1710435481828-124680f5a3e9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHZpbGxhcyUyMGF0JTIwZXZlbmluZ3xlbnwwfHwwfHx8MA%3D%3D')"}} id='Header'>
+      <Navbar/>
+      <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
+        <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Explore homes that fit your dreams </h2>
+        <div className='space-x-6 mt-16'>
+            <a href="#" className='border border-white px-8 py-3 rounded'>Projects</a>
+            <a href="#" className='border bg-blue-500 px-8 py-3 rounded'>Contact Us</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
