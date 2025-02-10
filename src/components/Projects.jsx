163ebea67b9693b7a7ebca3af9859f5c3c04f097
Piyush1532 <piyushadav503@gmail.com>
2025-02-10 13:27:34 +0530

    component not yet complete

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='projects'>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2'>Projects <span>Completed</span> </h1>
+      <p>Crafting Spaces , Building Legacies_Explore Our PortFolio</p>
+    </div>
+  )
+}
+
+export default Projects
