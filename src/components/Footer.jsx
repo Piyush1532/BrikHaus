95bf2621e8d324dbf47d53fd995fff8c5a495e57
Piyush1532 <piyushadav503@gmail.com>
2025-02-14 15:06:30 +0530

    animation added & component ready

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,17 +9,31 @@ const Footer = () => {
             <img src={assets.brikhaus_logo} alt="" className='w-[110px]'/>
             <p className='text-gray-400 mt-4' >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium, omnis fuga. Iusto porro dolorum unde sequi eos dignissimos, delectus architecto .</p>
         </div>
-        <div>
+        <div className='w-full md:w-1/5 mb-8 md:mb-0'>
             <h3 className='text-white text-lg font-bold mb-4'>Company</h3>
-            <ul>
-                <a href="#Header">Home</a>
+            <ul className='flex flex-col gap-2 text-gray-400'>
+                <a href="#Header" className='hover:text-white'>Home</a>
+                <a href="#about" className='hover:text-white'>About Us </a>
+                <a href="#contact" className='hover:text-white'>Contact us</a>
+                <a href="#" className='hover:text-white'>Privacy Policy</a>
             </ul>
         </div>
-        <div></div>
+        <div className='w-full md:w-1/3 '>
+        <h3 className='text-white text-lg font-bold mb-4'>Subscribe To Our Newsletter </h3>
+        <p className='text-gray-400 mb-4 max-w-80'>The latest news,articles and resources sent to your inbox weekly.</p>
+
+<div className='flex gap-2'>
+  <input type="email" placeholder='Enter your email' className='p-2 rounded bg-gray-800 text-gray-400 border border-gray-700 focus:outline-none w-full md:w-auto'  />
+  <button className='py-2 px-2 rounded bg-blue-500 text-white '>Subscribe</button>
+</div>
+
+        </div>
       </div>
 
 
-      <div></div>
+      <div className='border-t border-r-gray-700 py-4 mt-10 text-center text-gray-500'>
+        Copyright 2025 &copy; PiWebs . All rights Reserved
+      </div>
     </div>
   )
 }
